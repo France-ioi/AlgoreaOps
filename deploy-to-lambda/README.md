4755dd49d1ff8e304cd52709ff4bb5e00b3ea68a
Damien Leroy <dleroy@smad.be>
2020-09-10 11:40:23 +0200

    full script which also update configs

--- deploy-to-lambda/README.md
+++ deploy-to-lambda/README.md
@@ -1,5 +1,5 @@
-- Generate the lambda code using `make lambda-archive` in `AlgoreaBackend`.
-- Copy `lambda.zip` to this directory
+- Generate the binary code using `make lambda-build` in `AlgoreaBackend`.
+- Copy `AlgoreaBackend-linux` to this directory
 - Run `deploy.sh [--profile <yourawsprofile>]`
 - Do not forget to apply db migrations if any
 
--- deploy-to-lambda/deploy.sh
+++ deploy-to-lambda/deploy.sh
@@ -1,26 +1,47 @@
 #!/bin/sh
 set -e
 
-aws s3 sync ./ s3://algorea-lambda-upload/lambda-code/ --exclude "*" --include "lambda.zip" $1 $2
-aws lambda update-function-code --function-name AlgoreaBackend --s3-bucket algorea-lambda-upload --s3-key lambda-code/lambda.zip --query 'LastUpdateStatus' $1 $2
-CONFIG=`aws lambda get-function-configuration --function-name AlgoreaBackend --output json --query 'Environment' $1 $2`
+function deploy()
+{
+  TAG=$1
+  ALLUSERSGRP=$2
+  TMPUSERSGRP=$3
+  APIPATH=$4
+  DBNAME=$5
+  AWSPROFILE="$6 $7" # may be empty
 
-# replace api path
-CONFIG_DEV=${CONFIG/\"\/api\/\"/\"\/devapi\/\"}
-CONFIG_PRO=${CONFIG/\"\/devapi\/\"/\"\/api\/\"}
+  FILENAME=lambda-$TAG.zip
 
-# replace db username
-CONFIG_DEV=${CONFIG_DEV/\"algorea_full\"/\"algorea\"}
-CONFIG_PRO=${CONFIG_PRO/\"algorea\"/\"algorea_full\"}
+  ## PREPARE THE ARCHIVE
+  INITPWD=`pwd`
+  rm -rf $FILENAME
+  TMPDIR=`mktemp -d -t algoreabackend-XXXXXXXXXX`
+  cp AlgoreaBackend-linux ./private_key.pem ./public_key.pem $TMPDIR/
+  mkdir -p $TMPDIR/conf
+  sed "s/ALLUSERS/$ALLUSERSGRP/" config.lambda.yaml | sed -e "s/TMPUSERS/$TMPUSERSGRP/" > $TMPDIR/conf/config.yaml
+  cd $TMPDIR
+  zip -r $INITPWD/$FILENAME .
+  cd $INITPWD
+  rm -rf $TMPDIR
 
+  ## DEPLOY ON AWS LAMBDA
 
-aws lambda update-function-configuration --function-name AlgoreaBackend --environment "${CONFIG_DEV}" --query 'LastUpdateStatus' $1 $2
-VER=`aws lambda publish-version --function-name AlgoreaBackend --query 'Version' $1 $2`
-VER=${VER//\"/}
-aws lambda update-alias --function-name AlgoreaBackend --name dev --function-version $VER $1 $2
+  aws s3 sync ./ s3://algorea-lambda-upload/lambda-code/ --exclude "*" --include "$FILENAME" $AWSPROFILE
+  aws lambda update-function-code --function-name AlgoreaBackend --s3-bucket algorea-lambda-upload --s3-key lambda-code/$FILENAME --query 'LastUpdateStatus' $AWSPROFILE
+  CONFIG=`aws lambda get-function-configuration --function-name AlgoreaBackend --output json --query 'Environment' $AWSPROFILE`
 
-aws lambda update-function-configuration --function-name AlgoreaBackend --environment "${CONFIG_PRO}" --query 'LastUpdateStatus' $1 $2
-VER=`aws lambda publish-version --function-name AlgoreaBackend --query 'Version' $1 $2`
-VER=${VER//\"/}
-aws lambda update-alias --function-name AlgoreaBackend --name prod --function-version $VER $1 $2
+  # replace api path and db name
+  APIPATH=${APIPATH/\//\\\/}
+  CONFIG=`echo $CONFIG | sed "s#\"ALGOREA_SERVER__ROOTPATH\": \"[\/a-z]*\"#\"ALGOREA_SERVER__ROOTPATH\": \"$APIPATH\"#" | sed "s/\"ALGOREA_DATABASE__DBNAME\": \"[_a-z]*\"/\"ALGOREA_DATABASE__DBNAME\": \"$DBNAME\"/"`
 
+  # update config,
+  aws lambda update-function-configuration --function-name AlgoreaBackend --environment "${CONFIG}" --query 'LastUpdateStatus' $AWSPROFILE
+  # attach version to alias
+  VER=`aws lambda publish-version --function-name AlgoreaBackend --query 'Version' $AWSPROFILE`
+  VER=${VER//\"/}
+  aws lambda update-alias --function-name AlgoreaBackend --name $TAG --function-version $VER $AWSPROFILE
+
+}
+
+# deploy dev 1 4 /devapi/ algorea $1 $2
+deploy prod 3 2 /api/ algorea_full $1 $2
