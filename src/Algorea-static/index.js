e95f9ce432d4cbcab3bf3c6b1813994f91c136fe
Damien Leroy <dleroy@smad.be>
2022-07-25 16:55:27 +0200

    static file serving: only serve index.html files

--- src/Algorea-static/index.js
+++ src/Algorea-static/index.js
@@ -2,65 +2,38 @@ var AWS = require('aws-sdk');
 
 exports.handler = function(event, context, callback) {
 
-	if (parseInt(process.env.DEBUG)) console.log('(debug) event: '+JSON.stringify(event));
-
+	var debug = !!parseInt(process.env.DEBUG);
 	var region = process.env.S3_REGION;
 	var bucket = process.env.S3_BUCKET;
 	var prefix = process.env.S3_PREFIX || '';
-	var key = decodeURI(event.path);
+	var path = decodeURI(event.path);
 
-	// direct bypass for the root index -- disabled as done directly by ALB
-	/*if (key.slice(-1) === '/' && !['/en/', '/fr/'].includes(key.slice(-4))) {
-		callback(null, {
-					statusCode: 200,
-					isBase64Encoded: false,
-						headers: {
-							'Content-Type': "text/html"
-						},
-						body: '<html><head><script>window.location.replace("en/");</script></head></html>'
-			});
-			return;
-	}*/
+	if (debug) console.log('[DEBUG] event: '+JSON.stringify(event));
 
-	// if the last part of the path does have dots (we assume it is a "directory"), redirect to the slash path
-	// (cannot just serve the content as the browser would be in the "wrong" level of the path)
-	const matchMissingSlash = key.match(/^.*\/([\w\d_\-]+)$/i);
-	if (matchMissingSlash != null) {
-		callback(null, {
-			statusCode: 302,
-			isBase64Encoded: false,
-			multiValueHeaders: {
-				'location': [ matchMissingSlash[1]+'/' ]
-			},
-			headers: {
-				'location': matchMissingSlash[1]+'/',
-			},
-		});
+	/* Compute the s3 path mid-fix (to be prepended with `prefix` and appended with `index.html`)
+	 * /branch/anything/even/with/slashes/en/anything/afterwards -> branch/anything/even/with/slashes/en/
+	 * /en/ -> en/
+	 * /fr/ -> fr/
+	 * /en/something/file -> en/
+	 * anything else -> error
+	 */
+	const sufixMatch = path.match(/^\/(branch\/.*\/)?(en|fr)\//i);
+	console.log(JSON.stringify(sufixMatch));
+	if (sufixMatch === null) {
+		console.error('Unable to deduce the s3 path prefix from path: '+path);
+		callback(null, notFoundResponse());
 		return;
 	}
+	var s3Key = prefix + (sufixMatch[1] || '') + sufixMatch[2]  + '/index.html';
 
-	if (key.slice(-1) === '/') key += 'index.html'; // if ending with a slash, get the index file
-	key = key.substring(1); // remove heading slash for S3
-	key = prefix + key; // if there is a prefix configured, prepend the path with the prefix
-
-	console.log('Getting file on S3 with path: '+key);
+	console.log('Getting file on S3 with path: '+s3Key);
 
 	var s3 = new AWS.S3({ region: region });
-	return s3.makeUnauthenticatedRequest('getObject', { Bucket: bucket, Key: key },	function(err, data) {
+	return s3.makeUnauthenticatedRequest('getObject', { Bucket: bucket, Key: s3Key }, function(err, data) {
 		if (err) {
-			console.log('Error from S3: '+JSON.stringify(err));
-			console.log('Region:'+region+' Bucket:'+bucket+" Key:"+key);
-			callback(null, {
-				statusCode: 404,
-				isBase64Encoded: false,
-				multiValueHeaders: { // if multivalue headers are enabled for alb
-					'Content-Type': [ 'text/html' ]
-				},
-				headers: {
-					'Content-Type': 'text/html'
-				},
-				body: "<html><body><h1>Not found.</h1><p>If the problem persists, please contact us.</p></body></html>"
-			});
+			console.error('Error from S3: '+JSON.stringify(err));
+			console.error('Region:'+region+' Bucket:'+bucket+" Key:"+s3Key);
+			callback(null, notFoundResponse());
 			return;
 		}
 		callback(null, {
@@ -76,3 +49,17 @@ exports.handler = function(event, context, callback) {
 		});
 	});
 };
+
+function notFoundResponse() {
+	return {
+		statusCode: 404,
+		isBase64Encoded: false,
+		multiValueHeaders: { // if multivalue headers are enabled for alb
+			'Content-Type': [ 'text/html' ]
+		},
+		headers: {
+			'Content-Type': 'text/html'
+		},
+		body: "<html><body><h1>Not found.</h1><p>If the problem persists, please contact us.</p></body></html>"
+	};
+}
