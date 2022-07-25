d0b53ccf6724d851f2ad615182a936b608b4422d
Damien Leroy <dleroy@smad.be>
2022-07-25 17:52:47 +0200

    use promise for better readibility

--- src/Algorea-static/index.js
+++ src/Algorea-static/index.js
@@ -1,12 +1,12 @@
 var AWS = require('aws-sdk');
 
-exports.handler = function(event, context, callback) {
+exports.handler = async function(event, context, callback) {
 
-	var debug = process.env.DEBUG && process.env.DEBUG !== '0';
-	var region = process.env.S3_REGION;
-	var bucket = process.env.S3_BUCKET;
-	var prefix = process.env.S3_PREFIX || '';
-	var path = decodeURI(event.path);
+	const debug = process.env.DEBUG && process.env.DEBUG !== '0';
+	const region = process.env.S3_REGION;
+	const bucket = process.env.S3_BUCKET;
+	const prefix = process.env.S3_PREFIX || '';
+	const path = decodeURI(event.path);
 
 	if (debug) console.log('[DEBUG] event: '+JSON.stringify(event));
 
@@ -24,30 +24,20 @@ exports.handler = function(event, context, callback) {
 		callback(null, notFoundResponse());
 		return;
 	}
-	var s3Key = prefix + (sufixMatch[1] || '') + sufixMatch[2]  + '/index.html';
+	const s3Key = prefix + (sufixMatch[1] || '') + sufixMatch[2]  + '/index.html';
 
 	console.log('Getting file on S3 with path: '+s3Key);
 
-	var s3 = new AWS.S3({ region: region });
-	return s3.makeUnauthenticatedRequest('getObject', { Bucket: bucket, Key: s3Key }, function(err, data) {
-		if (err) {
+	const s3 = new AWS.S3({ region: region });
+	return s3
+		.makeUnauthenticatedRequest('getObject', { Bucket: bucket, Key: s3Key })
+		.promise()
+		.then(successResponse)
+		.catch(err => {
 			console.error('Error from S3: '+JSON.stringify(err));
 			console.error('Region:'+region+' Bucket:'+bucket+" Key:"+s3Key);
-			callback(null, notFoundResponse());
-			return;
-		}
-		callback(null, {
-			statusCode: 200,
-			multiValueHeaders: { // if multivalue headers are enabled for alb
-				'Content-Type': [ data.ContentType ]
-			},
-			headers: { // if multivalue headers are disabled for alb
-				'Content-Type': data.ContentType,
-			},
-			body: Buffer.from(data.Body).toString(),
-			isBase64Encoded: false
+			return notFoundResponse();
 		});
-	});
 };
 
 function notFoundResponse() {
@@ -62,4 +52,18 @@ function notFoundResponse() {
 		},
 		body: "<html><body><h1>Not found.</h1><p>If the problem persists, please contact us.</p></body></html>"
 	};
+}
+
+function successResponse(s3Data) {
+	return {
+			statusCode: 200,
+			multiValueHeaders: { // if multivalue headers are enabled for alb
+				'Content-Type': [ s3Data.ContentType ]
+			},
+			headers: { // if multivalue headers are disabled for alb
+				'Content-Type': s3Data.ContentType,
+			},
+			body: Buffer.from(s3Data.Body).toString(),
+			isBase64Encoded: false
+		};
 }
