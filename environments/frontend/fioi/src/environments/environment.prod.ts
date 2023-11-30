81c79a268424f0a39f56f6cfab177001d08e4ced
Damien Leroy <dleroy@smad.be>
2023-11-30 13:41:38 +0100

    auth key for apps

--- environments/frontend/fioi/src/environments/environment.prod.ts
+++ environments/frontend/fioi/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment: Environment = {
   production: true,
   apiUrl: '/api',
   oauthServerUrl: 'https://login.france-ioi.org',
-  oauthClientId: '43',
+  oauthClientId: '56',
   searchApiUrl: 'https://jyz57q4k3ytekopv6tvg5bdxaq0vlgso.lambda-url.eu-west-3.on.aws/',
 
   sentryDsn: 'https://6295834d69104f54b55cc0ebe4ada310@o1167067.ingest.sentry.io/6257761',
