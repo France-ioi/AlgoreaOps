c701caeca9705ee01ba7cf2bfa97dca0455c3889
Damien Leroy <dleroy@smad.be>
2023-12-01 14:30:44 +0100

    setup forum in fioi frontend

--- environments/frontend/fioi/src/environments/environment.prod.ts
+++ environments/frontend/fioi/src/environments/environment.prod.ts
@@ -6,6 +6,7 @@ export const environment: Environment = {
   oauthServerUrl: 'https://login.france-ioi.org',
   oauthClientId: '56',
   searchApiUrl: 'https://jyz57q4k3ytekopv6tvg5bdxaq0vlgso.lambda-url.eu-west-3.on.aws/',
+  forumServerUrl: 'wss://0j8i1y8ca2.execute-api.eu-west-3.amazonaws.com/fioi',
 
   sentryDsn: 'https://6295834d69104f54b55cc0ebe4ada310@o1167067.ingest.sentry.io/6257761',
 
