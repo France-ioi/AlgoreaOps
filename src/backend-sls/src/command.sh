5ce9fb03dc8f142e6c3b213e85d04b94b8c73d7e
Damien Leroy <dleroy@smad.be>
2024-04-03 11:31:49 +0200

    try to write in tmp dir instead

--- src/backend-sls/src/command.sh
+++ src/backend-sls/src/command.sh
@@ -20,9 +20,9 @@ do
   COMMAND=$(echo ${EVENT_DATA} | cut -d\" -f4) # extract the first string from the data
   echo "Extracted command: ${COMMAND}"
   if [[ "$COMMAND" =~ ^(db-recompute|db-migrate|db-migrate-undo|delete-temp-users|propagation)$ ]]; then
-    echo "" > $LAMBDA_TASK_ROOT/output.txt
-    OUTPUT=`$LAMBDA_TASK_ROOT/AlgoreaBackend ${COMMAND} | tee $LAMBDA_TASK_ROOT/output.txt`
-    curl "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/response" --data-binary "@$LAMBDA_TASK_ROOT/output.txt" > /dev/null
+    echo "" > /tmp/output.txt
+    OUTPUT=`$LAMBDA_TASK_ROOT/AlgoreaBackend ${COMMAND} | tee /tmp/output.txt`
+    curl "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/response" --data-binary "@/tmp/output.txt" > /dev/null
   else
     curl "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/error"  -d "Invalid/unauthorized data: ${EVENT_DATA}" > /dev/null
   fi
