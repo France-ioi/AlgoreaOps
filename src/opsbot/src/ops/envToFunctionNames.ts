3df6dc871824f2a02e771c5b9517e49ae2c21275
Damien Leroy <dleroy@smad.be>
2024-05-08 17:19:15 +0200

    add line which looks wrong

--- src/opsbot/src/ops/envToFunctionNames.ts
+++ src/opsbot/src/ops/envToFunctionNames.ts
@@ -2,6 +2,5 @@
 export function lambdaFunctionName(app: string, deployEnv: string): string {
   if (app === 'frontend') return `alg-frontend-${deployEnv}-static-serve`;
   if (app === 'backend') return `alg-backend-${deployEnv}-server`;
-  if (app === 'backend-command') return `alg-backend-${deployEnv}-server`;
   throw new Error('unsupported app: '+app);
 }
