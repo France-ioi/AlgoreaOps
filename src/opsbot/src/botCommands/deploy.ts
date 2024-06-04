5afd0a2fbb56ae65734cb963c2fa1ae4441f9e41
Damien Leroy <dleroy@smad.be>
2024-06-04 16:37:46 +0200

    bot: allow not proving the config

--- src/opsbot/src/botCommands/deploy.ts
+++ src/opsbot/src/botCommands/deploy.ts
@@ -1,7 +1,7 @@
 import { Task } from '../workerTasks/tasks';
 
 export function parseDeploy(channel: string, text: string): Task|undefined {
-  const match = /^deploy (frontend|backend) (fioi-prod) ([\d.-]*) ([A-Fa-f0-9]{6})+$/.exec(text);
+  const match = /^deploy (frontend|backend) (fioi-prod) ([\d.-]*)(?: ([A-Fa-f0-9]{1,41}))?$/.exec(text);
   if (match !== null) {
     if (!match[1] || !match[2] || !match[3]) throw new Error('unexpected: no arg match');
     return {
