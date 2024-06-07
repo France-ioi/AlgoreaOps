38d860ebc5d68e791bb118ec79346abe3e011e55
Damien Leroy <dleroy@smad.be>
2024-06-07 11:26:58 +0200

    handle more properly the stream error/end handing

--- src/opsbot/src/handlers/bot.ts
+++ src/opsbot/src/handlers/bot.ts
@@ -31,13 +31,11 @@ export const streamHandler = awslambda.streamifyResponse(async (event, responseS
 
     if (body.type === 'url_verification') {
       if (!body.challenge) throw { errorMessage: 'missing challenge', body: 'received: '+event.body };
-      responseStream.write(JSON.stringify({ challenge: body.challenge }));
-      responseStream.end();
+      responseStream.end(JSON.stringify({ challenge: body.challenge }));
     } else if (body.type === 'event_callback') {
       if (body.event === undefined) throw { errorMessage: 'missing body event', body: 'received: '+event.body };
       if ((body.event as { type: string }).type === 'message') {
-        responseStream.write('{}');
-        responseStream.end();
+        responseStream.end('{}');
         await handleSlackMessageEvent(body.event as Message);
       } else {
         throw { errorMessage: 'unrecognised event type', type: body.type };
@@ -45,9 +43,11 @@ export const streamHandler = awslambda.streamifyResponse(async (event, responseS
     } else throw { errorMessage: 'unsupported type', type: body.type };
   } catch (e) {
     // eslint-disable-next-line no-console
-    console.error(JSON.stringify(e));
-    responseStream.write(JSON.stringify(e));
-    responseStream.end();
+    console.error(e);
+    if (responseStream.writable) {
+      responseStream.write(JSON.stringify(e));
+      responseStream.end();
+    }
   }
 });
 
