a8a258e1c5fc30c9949ee60d23f7d3fdc2f3f6ab
Damien Leroy <dleroy@smad.be>
2024-06-11 11:45:31 +0200

    remove the ops worker as not useful anymore

--- src/opsbot/src/workerTasks/parse.ts
+++ src/opsbot/src/workerTasks/parse.ts
@@ -1,4 +1,3 @@
-import { textStatus } from './status';
 import { SlackChatClient } from '../libs/slackChatClient';
 import { Task } from '../tasks/tasks';
 
@@ -7,10 +6,6 @@ export async function parseAction(task: Task): Promise<void> {
   if (!channel) throw new Error('error: missing slack channel config');
   const slackClient = new SlackChatClient(channel, 'worker');
 
-  if (task.action === 'printStatus') {
-    await slackClient.send(await textStatus());
-  } else {
-    await slackClient.send(`Invalid task: ${JSON.stringify(task)}`);
-    throw new Error(`The input event is not a supported "Task": ${JSON.stringify(task)}`);
-  }
+  await slackClient.send(`Invalid task: ${JSON.stringify(task)}`);
+  throw new Error(`The input event is not a supported "Task": ${JSON.stringify(task)}`);
 }
