a3f3a534297cf63fa94ab7f2d0afcaf14bcc9160
Damien Leroy <dleroy@smad.be>
2024-06-21 16:22:01 +0200

    setup opsbot for dev aws account

--- src/opsbot/src/ops/s3deployments.ts
+++ src/opsbot/src/ops/s3deployments.ts
@@ -4,9 +4,11 @@ import { awsConfig } from '../libs/awsConfig';
 type LambdaVersion = string;
 
 export async function getLambdaVersion(app: string, deployEnv: string, fullVersion: string): Promise<LambdaVersion> {
+  const opsBucket = process.env['OPS_BUCKET'];
+  if (!opsBucket) throw new Error('OPS_BUCKET must be set');
   const client = new S3Client(awsConfig);
   const key = `deployments/${app}/${deployEnv}/${fullVersion}/LAMBDA_VERSION`;
-  const command = new GetObjectCommand({ Bucket: 'alg-ops', Key: key });
+  const command = new GetObjectCommand({ Bucket: opsBucket, Key: key });
   const data = await client.send(command);
   if (!data.Body) throw new Error('missing "Body" key in S3 get object response');
   const fileContent = await data.Body.transformToString();
