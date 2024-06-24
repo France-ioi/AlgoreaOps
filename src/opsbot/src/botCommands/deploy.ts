4ab79dcfb33e8d2652ba31db128836bf3151890d
Damien Leroy <dleroy@smad.be>
2024-06-24 11:00:23 +0200

    fix regex matching using string templates

--- src/opsbot/src/botCommands/deploy.ts
+++ src/opsbot/src/botCommands/deploy.ts
@@ -2,8 +2,6 @@ import { Task } from '../tasks/tasks';
 
 export function parseDeploy(text: string): Task|undefined {
   const regex = new RegExp(String.raw`^deploy (frontend|backend) (${process.env['ALLOWED_DEPLOYENV']}) ([\d.-]+)(?: ([A-Fa-f0-9]{1,41}))?$`);
-  
-  console.log(`deploy regex: ${regex.source}`);
   const match = regex.exec(text);
   if (match !== null) {
     if (!match[1] || !match[2] || !match[3]) throw new Error('unexpected: no arg match');
--- src/opsbot/src/botCommands/release.ts
+++ src/opsbot/src/botCommands/release.ts
@@ -1,7 +1,7 @@
 import { Task } from '../tasks/tasks';
 
 export function parseRelease(text: string): Task|undefined {
-  const regex = new RegExp(`^release (frontend|backend) (${process.env['ALLOWED_DEPLOYENV']}) (\d{1,3}.\d{1,3}.\d{1,3}-[A-Fa-f0-9]{7}-[A-Fa-f0-9]{7})$`);
+  const regex = new RegExp(String.raw`^release (frontend|backend) (${process.env['ALLOWED_DEPLOYENV']}) (\d{1,3}.\d{1,3}.\d{1,3}-[A-Fa-f0-9]{7}-[A-Fa-f0-9]{7})$`);
   const releaseMatch = regex.exec(text);
   if (releaseMatch !== null) {
     if (!releaseMatch[1] || !releaseMatch[2] || !releaseMatch[3]) throw new Error('unexpected: no arg match');
